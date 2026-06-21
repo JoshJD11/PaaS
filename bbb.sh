cff7d88c9c03ab4d1d7f619a693eaa333fe83318
Sebas Quesada <167464285+Sebco27@users.noreply.github.com>
2026-06-21 15:26:01 -0600

    add pull option to bbb.sh
    The .gitmodules file is no longer required because BBB-Docker files are now fetched as a regular repository through `git clone --recurse-submodules`

--- bbb.sh
+++ bbb.sh
@@ -2,6 +2,7 @@
 
 if [ -z "$1" ]; then
   echo "Uso:"
+  echo "  $0 --pull    Importa BBB-Docker con sus submodulos"
   echo "  $0 --build   Construye y ejecuta el entorno"
   echo "  $0 --up      Levanta los servicios (en modo detach)"
   echo "  $0 --down    Detiene los servicios"
@@ -9,24 +10,26 @@ if [ -z "$1" ]; then
   exit 1
 fi
 
-cd bbb-docker || exit 1
+cd bbb-dev || exit 1
 
 case "$1" in
+  --pull)
+    git clone --recurse-submodules https://github.com/Sebco27/bbb-docker.git bbb-docker
+    ;;
   --build)
-    sudo ./scripts/dev
+    sudo ./scripts/build.sh
     ;;
   --up)
     sudo docker compose up -d
+    EXTERNAL_IPv4=$(ip route get 8.8.8.8 | head -1 | awk '{ print $7 }')
+    echo "============================================"
+    echo "BBB Development Server"
+    echo ""
+    echo "API: https://mconf.github.io/api-mate/#server=https://${EXTERNAL_IPv4}/bigbluebutton/api&sharedSecret=SuperSecret"
+    echo "============================================"
     ;;
   --down)
     sudo docker compose down
     ;;
 esac
 
-EXTERNAL_IPv4=$(ip route get 8.8.8.8 | head -1 | awk '{ print $7 }')
-
-echo "============================================"
-echo "BBB Development Server"
-echo ""
-echo "API: https://mconf.github.io/api-mate/#server=https://${EXTERNAL_IPv4}/bigbluebutton/api&sharedSecret=SuperSecret"
-echo "============================================"
